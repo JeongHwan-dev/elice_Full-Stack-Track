cb0534652b8d6f86614a7761ffd0620687157285
Park Jeong Hwan <jeonghwan.dev@gmail.com>
2021-02-04 23:37:48 +0900

    06-01 commit

---
+++ Week6-Web_Front-end_1/[06-01] JavaScript/Ch01-DOM/02-Change_the_style/js/index.js
@@ -0,0 +1,11 @@
+// 아이디가 "apple"인 요소를 선택하도록 코드를 작성하세요.
+var selectedId = document.getElementById("apple");
+
+// 선택한 요소의 색깔을 "red"로 바꿉니다.
+selectedId.style.color = "red";
+
+// 아이디가 "banana"인 요소를 선택하도록 코드를 작성하세요.
+var selectedId2 = document.getElementById("banana");
+
+// 선택한 요소의 색깔을 "yellow"로 바꿉니다.
+selectedId2.style.color = "yellow";
---
+++ Week6-Web_Front-end_1/[06-01] JavaScript/Ch01-DOM/03-Change_the_content/js/index.js
@@ -0,0 +1,11 @@
+// 아이디가 "apple"인 요소를 선택하도록 코드를 작성하세요.
+var selectedId = document.getElementById("apple");
+
+// 선택한 요소의 내용을 "strawberry"로 바꿉니다.
+selectedId.innerHTML = "strawberry";
+
+// 아이디가 "onion"인 요소를 선택하도록 코드를 작성하세요.
+var selectedId2 = document.getElementById("onion");
+
+// 선택한 요소의 내용을 "garlic" 바꿉니다.    
+selectedId2.innerHTML = "garlic";
