7052a2c028f2bf67643dc73f520e6fbfaaa2d191
Park Jeong Hwan <jeonghwan.dev@gmail.com>
2021-02-06 15:01:46 +0900

    06-02 commit

---
+++ Week6-Web_Front-end_1/[06-02] React-1/Ch03-Component/react-class-component/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// 클래스 컴포넌트
+
+// 정의된 이름
+const name = "Sara";
+
+// 클래스명이 Welecome인 컴포넌트를 작성합니다.
+class Welcome extends React.Component {
+  render() {
+    return <h2>Welecome, {name}!</h2>
+  }
+}
+
+// 컴포넌트를 호출하여 element에 저장합니다.
+const element = <Welcome />;
+
+ReactDOM.render(element, document.getElementById('root'));
