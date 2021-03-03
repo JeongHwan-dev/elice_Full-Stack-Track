3813e2ece27a0cdd4da5032b85712931d5e22110
Park Jeong Hwan <jeonghwan.dev@gmail.com>
2021-03-03 23:33:38 +0900

    06-04 commit

--- Week6-Web_Front-end_1/[06-04] React-2/Ch02-State/04-react-lifecycle-method/src/index.js
+++ Week6-Web_Front-end_1/[06-04] React-2/Ch02-State/04-react-lifecycle-method/src/index.js
@@ -1,34 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
 class Clock extends React.Component {
+  // 1번째 호출
   constructor(props) {
     console.log("constructor() 호출");
     super(props);
-    this.state = {date: new Date()};
+    this.state = { date: new Date() };
   }
 
+  // 3번째 호출
   componentDidMount() {
     console.log("componentDidMount() 호출");
     this.timerID = setInterval(() => this.tick(), 1000);
   }
 
+  // 컴포넌트가 마운트 해제되어 제거되기 직전에 호출
   componentWillUnmount() {
     console.log("componentWillUnmount() 호출");
     clearInterval(this.timerID);
   }
 
+  // 4번째 호출
   tick() {
     console.log("tick() 호출");
-    this.setState({date: new Date()});
+    this.setState({ date: new Date() });
   }
 
+  // 2번째 호출
   render() {
     console.log("render() 호출");
-    return(
+    return (
       <div>
         <h1>Hello, World!</h1>
         <h2>It id {this.state.date.toLocaleTimeString()}</h2>
@@ -37,4 +40,4 @@ class Clock extends React.Component {
   }
 }
 
-ReactDOM.render(<Clock />, document.getElementById('root'));
+ReactDOM.render(<Clock />, document.getElementById("root"));
