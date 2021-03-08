95f6a1d86e34319fe4a7dfc45b5bc0dbea557b68
Park Jeong Hwan <jeonghwan.dev@gmail.com>
2021-03-08 21:22:07 +0900

    06-04 commit

--- Week6-Web_Front-end_1/[06-04] React-2/Ch03-Event_Key/01-react-event/src/index.js
+++ Week6-Web_Front-end_1/[06-04] React-2/Ch03-Event_Key/01-react-event/src/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
 function ActionLink() {
   // 이벤트를 등록합니다.
   function handleClick(e) {
     console.log("The link was clicked.");
-    alert("버튼이 클릭되었습니다.")
+    alert("버튼이 클릭되었습니다.");
   }
   // a 태그에 이벤트를 등록합니다.
   return (
-    <a href="#" onClick={handleClick}>Click me</a>
+    <a href="#" onClick={handleClick}>
+      Click me
+    </a>
   );
 }
 
-ReactDOM.render(<ActionLink />, document.getElementById('root'));
+ReactDOM.render(<ActionLink />, document.getElementById("root"));
